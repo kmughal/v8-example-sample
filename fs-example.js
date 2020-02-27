763dc06667a71bce9c4534721e2fde0ffca3599b
K.H.U.R.R.A.M <kmughal@gmail.com>
2020-02-27 18:05:19 +0000

    change fs-example

--- fs-example.js
+++ fs-example.js
@@ -1,9 +1,14 @@
 log('Number of cors in your computer :' + cors)
 
-readFile('path of the file', (err, fileContents) => {
-  if (err) {
-    log('Something went wrong : ' + err)
-    return
+readFile(
+  '/Users/khurram/Documents/Code/source-codes/depot_tools/v8/samples/sample.cc',
+  (err, fileContents) => {
+    if (err) {
+      log('Something went wrong : ' + err)
+      return
+    }
+   //log(fileContents);
+    helloWorld().then(v => log(v));
   }
-  log(fileContents)
-})
+)
+
